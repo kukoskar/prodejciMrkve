11e4eaa67251420932a83ed9a8390ac1829a8f25
kukoskar <47847372+kukoskar@users.noreply.github.com>
2023-02-10 11:15:30 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.time.LocalDate;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        String jmeno = "Milan", prijmeni = "Borovicka", mesto = "Praha", spz = "4AC 4698";
+        LocalDate datumNarozeni = LocalDate.of(2002, 5, 12);
+        double pocetSmluv = 20, prodanaMrkev = 50, spotrebaVozidlaNa100km = 11.5, prumer;
+        prumer = prodanaMrkev / pocetSmluv;
+        String IPv4 = "192.168.48.38";
+
+        System.out.println("Průměrné množství prodané mrkve na jednu smlouvu je : " + prumer + " tuny");
     }
 }
